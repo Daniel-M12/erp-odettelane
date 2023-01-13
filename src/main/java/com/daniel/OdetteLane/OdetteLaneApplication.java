ffaeb77a102d7afe49de96c4f7f04cdcd111a165
Daniel Matos <deme12.1@gmail.com>
2023-01-13 07:54:54 -0500

    Inicializando repositorio

---
+++ src/main/java/com/daniel/OdetteLane/OdetteLaneApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.OdetteLane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdetteLaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdetteLaneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daniel/OdetteLane/OdetteLaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.OdetteLane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdetteLaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

34a36d0ad0c84f3ebefdf40c06351d3172471287
Daniel Matos <deme12.1@gmail.com>
2023-02-17 07:19:15 -0500

    Creada Entity Colour y CrudRepository

---
+++ src/main/java/com/daniel/OdetteLane/domain/entities/fabric/Colour.java
@@ -0,0 +1,22 @@
+package com.daniel.OdetteLane.domain.entities.fabric;
+
+public class Colour {
+    private int colourId;
+    private String colour;
+
+    public int getColourId() {
+        return colourId;
+    }
+
+    public void setColourId(int colourId) {
+        this.colourId = colourId;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+}
---
+++ src/main/java/com/daniel/OdetteLane/persistence/crud/tela/ColorCrudRepository.java
@@ -0,0 +1,7 @@
+package com.daniel.OdetteLane.persistence.crud.tela;
+
+import com.daniel.OdetteLane.persistence.entity.infoTela.Color;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ColorCrudRepository extends CrudRepository<Color,Integer> {
+}
